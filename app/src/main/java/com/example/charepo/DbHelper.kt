71655b38ee61028d6a8384cead49ff1e1d63a800
Dbaker-01 <112969142+Dbaker-01@users.noreply.github.com>
2023-04-04 14:44:53 -0400

    Update DbHelper.kt

--- app/src/main/java/com/example/charepo/DbHelper.kt
+++ app/src/main/java/com/example/charepo/DbHelper.kt
@@ -3,6 +3,7 @@ package com.example.charepo
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import android.media.Image
 
 class DbHelper(context: Context) : SQLiteOpenHelper(context, "Charepo", null, 1){
     override fun onCreate(db: SQLiteDatabase?) {
@@ -13,5 +14,15 @@ class DbHelper(context: Context) : SQLiteOpenHelper(context, "Charepo", null, 1)
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
 
+    }
+
+    fun AddNewCharacter(CName: String, CDescription: String, CPicture:Image, CPrivate: Boolean, db: SQLiteDatabase?)
+    {
+        db?.execSQL("INSERT INTO Character(CName, CDescription, CPicture, CPrivate) VALUES(CName,CDescription,CPicture, CPrivate)")
+    }
+
+    fun AddNewAccount(EMAIL: String, PASSWORD: String,LoggedIn: Boolean, db: SQLiteDatabase?)
+    {
+        db?.execSQL("INSERT INTO AccountInformation(UserName, Password,LoggedIn) VALUES(EMail,PASSWORD,LoggedIn)")
     }
 }
